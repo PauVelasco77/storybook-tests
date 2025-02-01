cd1c03b40b3b667f8a41b24380a3bbbcf3c85aba
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-02-02 00:04:02 +0100

    add vitest configuration and setup for testing with coverage support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {coverageConfigDefaults, defineConfig} from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // Enables global `test`, `expect`, etc.
+    environment: "jsdom", // 👈 Ensures a browser-like environment
+    setupFiles: "./setupTests.ts", // 👈 Ensures setupTests.ts is loaded
+    coverage: {
+      // 👇 Add this
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "**/.storybook/**",
+        // 👇 This pattern must align with the `stories` property of your `.storybook/main.ts` config
+        "**/*.stories.*",
+        // 👇 This pattern must align with the output directory of `storybook build`
+        "**/storybook-static/**",
+      ],
+    },
+  },
+});
