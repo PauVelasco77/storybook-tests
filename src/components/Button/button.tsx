bca268bb14af7de0fb14862c7c04b10729b425d3
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-02-08 09:25:35 +0000

    enhance Button component with loading state and loader animation

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,7 +1,29 @@
+import {ButtonHTMLAttributes} from "react";
+import {Loader2} from "lucide-react";
 import "./button.css";
 
-export type ButtonProps = React.ComponentPropsWithoutRef<"button">;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isLoading?: boolean;
+}
 
-export default function Button(props: ButtonProps) {
-  return <button className='button' {...props} />;
+export default function Button({
+  isLoading,
+  children,
+  className,
+  disabled,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={`button ${isLoading ? "button__loading" : ""} ${
+        className || ""
+      }`}
+      disabled={disabled || isLoading}
+      {...props}>
+      {isLoading && <Loader2 className='button--loader' />}
+      <span className={isLoading ? "button--hidden-content" : ""}>
+        {children}
+      </span>
+    </button>
+  );
 }
