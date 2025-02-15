cfa8531c09045475af55d51fef8ad6c3392e9c7a
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-02-15 14:13:20 +0100

    refactor TextField component styles and improve class name handling

--- src/components/TextField/textField.tsx
+++ src/components/TextField/textField.tsx
@@ -1,7 +1,10 @@
+import cx from "classnames";
 import "./textField.css";
 
 export type TextFieldProps = React.ComponentPropsWithoutRef<"input">;
 
 export default function TextField(props: TextFieldProps) {
-  return <input className='text-field' {...props} />;
+  const textFieldClassNames = cx("text-field", "body-lg", props.className);
+
+  return <input className={textFieldClassNames} {...props} />;
 }
