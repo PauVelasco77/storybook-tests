5ecdff2825fba31395b9a081c9ec36ec3bb11751
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-02-27 15:15:07 +0100

    refactor TextField component to enhance accessibility and styling, including improved label handling and error state management

--- src/components/TextField/textField.stories.ts
+++ src/components/TextField/textField.stories.ts
@@ -8,6 +8,7 @@ const meta = {
   parameters: {
     layout: "centered",
   },
+  argTypes: {},
   tags: ["autodocs"],
 } satisfies Meta<typeof TextField>;
 
@@ -16,14 +17,20 @@ type Story = StoryObj<typeof meta>;
 
 export const Text: Story = {
   args: {
+    labelText: "Name",
     placeholder: "Enter your name",
     type: "text",
+    id: "name",
+    errorText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque veniam, harum praesentium facere perferendis ut eveniet adipisci reiciendis sed! Ipsum dolor magnam officia consectetur quos sint quibusdam, quaerat expedita!",
   },
 };
 
 export const Password: Story = {
   args: {
+    labelText: "Password",
     placeholder: "Enter your password",
     type: "password",
+    id: "password",
   },
 };
--- src/components/TextField/textField.tsx
+++ src/components/TextField/textField.tsx
@@ -1,10 +1,86 @@
+import {forwardRef} from "react";
 import cx from "classnames";
 import "./textField.css";
+import {TextFieldTypeType} from "./textField.types";
 
-export type TextFieldProps = React.ComponentPropsWithoutRef<"input">;
-
-export default function TextField(props: TextFieldProps) {
-  const textFieldClassNames = cx("text-field", "body-lg", props.className);
-
-  return <input className={textFieldClassNames} {...props} />;
+interface TextFieldProps
+  extends Omit<React.ComponentPropsWithoutRef<"input">, "type"> {
+  type: TextFieldTypeType;
+  labelText: string;
+  errorText?: string;
+  required?: boolean;
+  hideLabel?: boolean;
+  hideError?: boolean;
 }
+
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  (
+    {
+      labelText,
+      errorText,
+      hideError = false,
+      required = false,
+      hideLabel = false,
+      id,
+      className,
+      disabled,
+      ...rest
+    },
+    ref
+  ) => {
+    const uniqueId =
+      id || `text-field-${Math.random().toString(36).slice(2, 11)}`;
+
+    const textFieldClassNames = cx(
+      "text-field",
+      {
+        "text-field--disabled": disabled,
+        "text-field--error": errorText,
+      },
+      className
+    );
+
+    return (
+      <div className={textFieldClassNames}>
+        <div className='text-field__input-container'>
+          <label
+            className={cx("label-md", {"sr-only": hideLabel})}
+            htmlFor={uniqueId}>
+            {labelText}
+            {required && (
+              <span className='text-field__required' aria-hidden='true'>
+                *
+              </span>
+            )}
+          </label>
+
+          <input
+            ref={ref}
+            id={uniqueId}
+            className='body-lg'
+            aria-invalid={!!errorText}
+            aria-describedby={errorText ? `${uniqueId}-description` : undefined}
+            required={required}
+            disabled={disabled}
+            {...rest}
+          />
+        </div>
+
+        {errorText && (
+          <p
+            id={`${uniqueId}-description`}
+            className={cx("text-field__helper-text", "body-sm", {
+              "text-field__helper-text--error": errorText,
+              "sr-only": hideError,
+            })}>
+            {errorText}
+          </p>
+        )}
+      </div>
+    );
+  }
+);
+
+TextField.displayName = "TextField";
+
+export default TextField;
