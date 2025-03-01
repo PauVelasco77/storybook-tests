dfe1dc7aa907258dfc5d7c492482edc5b82f2a2a
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-03-01 17:00:33 +0100

    refactor TextField component to use default export and simplify props handling

--- src/components/TextField/textField.tsx
+++ src/components/TextField/textField.tsx
@@ -1,7 +1,6 @@
-import {forwardRef} from "react";
+import {TextFieldTypeType} from "./textField.types";
 import cx from "classnames";
 import "./textField.css";
-import {TextFieldTypeType} from "./textField.types";
 
 interface TextFieldProps
   extends Omit<React.ComponentPropsWithoutRef<"input">, "type"> {
@@ -12,72 +11,62 @@ interface TextFieldProps
   hideError?: boolean;
 }
 
-export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  (
+export default function TextField({
+  label,
+  errorText,
+  hideError = false,
+  required = false,
+  hideLabel = false,
+  id,
+  className,
+  disabled,
+  type,
+  ...rest
+}: TextFieldProps) {
+  const uniqueId = id || crypto.randomUUID();
+
+  const textFieldClassNames = cx(
+    "text-field",
     {
-      label,
-      errorText,
-      hideError = false,
-      required = false,
-      hideLabel = false,
-      id,
-      className,
-      disabled,
-      type,
-      ...rest
+      "text-field--disabled": disabled,
+      "text-field--error": errorText,
     },
-    ref
-  ) => {
-    const uniqueId = id || crypto.randomUUID();
+    className
+  );
 
-    const textFieldClassNames = cx(
-      "text-field",
-      {
-        "text-field--disabled": disabled,
-        "text-field--error": errorText,
-      },
-      className
-    );
+  return (
+    <div className={textFieldClassNames}>
+      <div className='text-field__input-container'>
+        <label
+          className={cx("label-md", {"sr-only": hideLabel})}
+          htmlFor={uniqueId}>
+          {label}
+          {required && <span className='text-field__required'>*</span>}
+        </label>
 
-    return (
-      <div className={textFieldClassNames}>
-        <div className='text-field__input-container'>
-          <label
-            className={cx("label-md", {"sr-only": hideLabel})}
-            htmlFor={uniqueId}>
-            {label}
-            {required && <span className='text-field__required'>*</span>}
-          </label>
-
-          <input
-            type={type}
-            ref={ref}
-            id={uniqueId}
-            className='body-lg'
-            aria-invalid={!!errorText}
-            aria-describedby={errorText ? `${uniqueId}-description` : undefined}
-            required={required}
-            disabled={disabled}
-            {...rest}
-          />
-        </div>
-
-        {errorText && (
-          <p
-            id={`${uniqueId}-description`}
-            role='alert'
-            className={cx("text-field__helper-text", "body-sm", {
-              "text-field__helper-text--error": errorText,
-              "sr-only": hideError,
-            })}>
-            {errorText}
-          </p>
-        )}
+        <input
+          type={type}
+          id={uniqueId}
+          className='body-lg'
+          aria-invalid={!!errorText}
+          aria-describedby={errorText ? `${uniqueId}-description` : undefined}
+          required={required}
+          disabled={disabled}
+          {...rest}
+        />
       </div>
-    );
-  }
-);
 
-TextField.displayName = "TextField";
-
-export default TextField;
+      {errorText && (
+        <p
+          id={`${uniqueId}-description`}
+          role='alert'
+          className={cx("text-field__helper-text", "body-sm", {
+            "text-field__helper-text--error": errorText,
+            "sr-only": hideError,
+          })}>
+          {errorText}
+        </p>
+      )}
+    </div>
+  );
+}
