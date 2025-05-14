4d02236fad38ea02916d687681caae12887f1e93
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-05-14 12:36:07 +0200

    add radix-ui dependencies and update configuration for improved functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,28 @@
-import {coverageConfigDefaults, defineConfig} from "vitest/config";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   test: {
     globals: true, // Enables global `test`, `expect`, etc.
-    environment: "jsdom", // 👈 Ensures a browser-like environment
-    setupFiles: "./setupTests.ts", // 👈 Ensures setupTests.ts is loaded
+    environment: 'jsdom', // 👈 Ensures a browser-like environment
+    setupFiles: './setupTests.ts', // 👈 Ensures setupTests.ts is loaded
     coverage: {
       // 👇 Add this
       exclude: [
         ...coverageConfigDefaults.exclude,
-        "**/.storybook/**",
+        '**/.storybook/**',
         // 👇 This pattern must align with the `stories` property of your `.storybook/main.ts` config
-        "**/*.stories.*",
+        '**/*.stories.*',
         // 👇 This pattern must align with the output directory of `storybook build`
-        "**/storybook-static/**",
+        '**/storybook-static/**',
       ],
     },
   },
